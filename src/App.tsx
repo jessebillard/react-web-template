b5a7d5e22d78b4032b1977f79033bbc897979c80
Jesse Billard <jesse.billard@flueid.com>
2022-08-08 21:13:41 -0400

    removes ugly test code

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
 import "./App.css";
 
 function App() {
-  const test = () => {
-    console.log("wowowoowowow");
-    const coo = "";
-
-    coo.toUpperCase();
-  };
-
   return (
     <>
       <h1>REACT WEB TEMPLATE</h1>
-
-      <div>WOWOWOWO</div>
     </>
   );
 }
